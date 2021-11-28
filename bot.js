5ffceb1400f82ed056ecf927e6fa8ed7841bcc4b
Sanjeev Kumar <82044212+krsanjeev196@users.noreply.github.com>
2021-11-28 20:34:42 +0000

    Added Multiple usernames. Created Embed and replied it to show output.

--- bot.js
+++ bot.js
@@ -1,51 +1,72 @@
 //const { Client, Intents } = require('discord.js');
+import { MessageEmbed } from 'discord.js';
+
 import {Client, Intents} from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const token = process.env['token'];
-//const fetch = require('node-fetch');
 import fetch from 'node-fetch';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+//creating embed to store final result
+const newEmbed = new MessageEmbed();
+
+newEmbed.setTitle('Codeforces submission count of last seven days.');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const codeforcesAccounts = [
+  {
+    name:"Sanjeev Kumar",
+    userid: "krsanjeev196"
+  },{
+    name:"Priyanshu Patwa",
+    userid: "priyanshu619"
+  },{
+    name:"Arohi Jain",
+    userid: "roejain"
+  },{
+    name:"Rudra",
+    userid: "rudra2901"
+  }
+]
+
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ziz') {
-    await interaction.reply('yo!');
-  } else if(interaction.commandName ==='hello')
+ if(interaction.commandName ==='hello')
   {
     await interaction.reply('Hello Friends!!\n You are talking to Ketchup.')
   }
-  else if(interaction.commandName ==='count'){
-    
-    const cf_api = `https://codeforces.com/api/user.status?handle=krsanjeev196&from=1&count=50`;
+  else if(interaction.commandName==='count'){
 
-    const response = await fetch(cf_api);
-    const jsonData= await response.json();
-    const lastWeekDate = (Date.now() /1000)-604800;
-    
-    console.log(lastWeekDate);
-    console.log(jsonData);
+    for(var j=0; j<codeforcesAccounts.length; j++)
+    {
+      let username = codeforcesAccounts[j].userid;
 
-    var count=0;
-    
-    for(var i=0; i<jsonData.result.length; i++){
-      if(jsonData.result[i].creationTimeSeconds >= lastWeekDate)
-          count++;
+      const cf_api = `https://codeforces.com/api/user.status?handle=${username}&from=1&count=50`;
+
+      const response = await fetch(cf_api);
+      const jsonData= await response.json();
+      const lastWeekDate = (Date.now() /1000)-604800;
+   
+      var count=0;
+      
+      for(var i=0; i<jsonData.result.length; i++){
+        if(jsonData.result[i].creationTimeSeconds >= lastWeekDate)
+            count++;
+      }
+
+      newEmbed.addFields(
+        {name: `${username}`, value:`${count}`}
+      )
+
     };
-     
-    console.log(count);
-
-    const username = 'krsanjeev196';
-    await interaction.reply(`${username}\n${count}\n`);
-
-
-    //await interaction.reply(`${count}\n`);
-    //await interaction.reply('This function is not working currentl :(');
+    
+    await interaction.reply({ embeds: [newEmbed] });  
   }
 });
-
 
 client.login(token);
--- index.js
+++ index.js
@@ -9,18 +9,18 @@
 
 import {REST} from '@discordjs/rest';
 import {Routes} from 'discord-api-types/v9';
+import promptSync from 'prompt-sync';
 
-// const { REST } = require('@discordjs/rest');
-// const { Routes } = require('discord-api-types/v9');
+const prompt = promptSync();
+//let username = prompt();
 
 const token = process.env['token']
 const CLIENT_ID = process.env['client_id']
 const GUILD_ID = process.env['guild_id']
 
+//var username;
+
 const commands = [{
-  name: 'ziz',
-  description: 'Replies with yo!'
-},{
   name :'hello',
   description : 'Greets!!'
 },{
@@ -28,10 +28,6 @@ const commands = [{
   description: 'Gives submisson of last week on Codeforces.'
 }]; 
 
-// const content = [{
-//   name :'hello',
-//   description : 'Greets!!'
-// }];
 
 console.log(CLIENT_ID)
 const rest = new REST({ version: '9' }).setToken(token);
@@ -54,22 +50,3 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 //-------------------------------
 
-//const client = new Discord.Client();
-// // import { Client, Intents , MessageEmbed}  from 'discord.js';
-// const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUIL  D_MESSAGES] });
-
-// console.log('Hello World!!');
-
-// client.on("ready", () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
-
-// client.on("message", msg => {
-//   if (msg.content === "Hello") {
-//     msg.reply("Hello Friends!!\n You are talking to ketchup.");
-//   }
-// });
-// console.log('Hello!!');
-
-//client.login(process.env.TOKEN);
-//client.login(`${TOKEN}`); 
